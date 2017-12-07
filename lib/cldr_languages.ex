2014384bb21cd256ba91c7ff8c2d93b9d93f2b27
Benjamin Milde <benni@kobrakai.de>
2017-12-07 14:53:26 +0100

    Initial commit

---
+++ lib/cldr_languages.ex
@@ -0,0 +1,18 @@
+defmodule CldrLanguages do
+  @moduledoc """
+  Documentation for CldrLanguages.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CldrLanguages.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cldr_languages_test.exs
@@ -0,0 +1,8 @@
+defmodule CldrLanguagesTest do
+  use ExUnit.Case
+  doctest CldrLanguages
+
+  test "greets the world" do
+    assert CldrLanguages.hello() == :world
+  end
+end
